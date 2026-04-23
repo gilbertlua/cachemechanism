84118a58cb8785cf1db8309cb5284884b622c839
gilbertlua <gilbertgmtidn@gmail.com>
2026-04-23 09:19:20 +0700

    refactor: implement type-safe generic cache with ICacheable constraint

--- Cache.Application/LargeData.cs
+++ Cache.Application/LargeData.cs
@@ -1,24 +1,15 @@
 using System.Data.Common;
-
+using Cache.Service;
 public class LargeData
 {
-    public User GetLargeData(string id)
+    public UserProfile GetLargeData(string id)
     {
         Thread.Sleep(3000);
         Console.WriteLine("Load from data source 3000ms");
-        return new User();
+        return new UserProfile()
+        {
+            Id = 2,
+            Name = "John"
+        };
     }
-    private List<User>? _users = new List<User>
-    {
-        new User { Id = 1, Name = "A" },
-        new User { Id = 2, Name = "B" },
-        new User { Id = 3, Name = "C" },
-        new User { Id = 4, Name = "D" },
-    };
-}
-
-public class User
-{
-    public int Id{get;set;}
-    public string? Name{get;set;}
 }
--- Cache.Application/Program.cs
+++ Cache.Application/Program.cs
@@ -4,13 +4,17 @@
 CacheStrategyBase _cache = new MemoryCacheStrategy();
 LargeData _dataManager = new LargeData();
 
-_cache.GetOrSet("1",() => _dataManager.GetLargeData("1"));
+_cache.GetOrSet("1", () => _dataManager.GetLargeData("1"));
 Console.WriteLine("First data completed");
-_cache.GetOrSet("2",() => _dataManager.GetLargeData("1"));
+_cache.GetOrSet("1",() => _dataManager.GetLargeData("1"));
 Console.WriteLine("Second data completed");
+// _cache.GetOrSet("1",() => _dataManager.GetLargeData("1"));
+// Console.WriteLine("First data completed");
+// _cache.GetOrSet("2",() => _dataManager.GetLargeData("1"));
+// Console.WriteLine("Second data completed");
 
-_cache.GetOrSet("1",() => _dataManager.GetLargeData("1"));
-Console.WriteLine("First data completed");
-_cache.GetOrSet("2",() => _dataManager.GetLargeData("1"));
-Console.WriteLine("Second data completed");
+// _cache.GetOrSet("1",() => _dataManager.GetLargeData("1"));
+// Console.WriteLine("First data completed");
+// _cache.GetOrSet("2",() => _dataManager.GetLargeData("1"));
+// Console.WriteLine("Second data completed");
 
